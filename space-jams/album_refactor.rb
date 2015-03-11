6bd221c911a24ebb390d83201cac28c98b505139
Meredith Fuhrman <meredith.fuhrman@gmail.com>
2015-03-11 14:54:04 -0400

    Final refactor with formatting

--- space-jams/album_refactor.rb
+++ space-jams/album_refactor.rb
@@ -8,30 +8,18 @@ class Album
     @tracks = Array.new
   end
 
-  def find_or_create(id, name, artists)
-    self.find { |a| a.id == track[:album_id] }
-    #
-    # # if the album hasn't been added to the albums array yet, add it
-    # if album.nil?
-    #   album = Album.new(track[:album_id], track[:album_name], track[:artists])
-    #   albums << album
-    # end
-
-    # add the track to the album's @tracks instance variable
-    # album.tracks << Track.new(track[:album_id], track[:track_id], track[:title], track[:track_number], track[:duration_ms])
-
-
-
+  def self.find_or_create(albums, id, name, artists)
+    album = albums.find { |a| a.id == id }
+    album ||= Album.new(id, name, artists)
+    album
   end
 
-
-  # Example method to show how to use select enumerable method
-  # def long_tracks
-  #   @tracks.select { |track| track.duration_ms > 240_000 }
-  # end
+  def add_track(track_id, title, number, duration)
+    @tracks << Track.new(track_id, title, number, duration)
+  end
 
   def track_list
-    @tracks.map { |track| track.title }.join("\t -") + "\n"
+    @tracks.map { |track| track.title }.join("\n-")
   end
 
   def duration_milliseconds
@@ -48,21 +36,27 @@ class Album
 
   def summary
     <<-summary
-    Name: #{@title}
-    Artist(s): #{@artists}
-    Duration (min): #{duration_min}
-    Tracks:
-    #{track_list}
+Name: #{@title}
+Artist(s): #{@artists}
+Duration (min): #{duration_min}
+Tracks:
+-#{track_list}
 
-    summary
+summary
   end
+
+  # Extra example method to show how to use select enumerable method
+  def long_tracks
+    @tracks.select { |track| track.duration_ms > 240_000 }
+  end
+
 end
+
 
 class Track
   attr_accessor :album_id, :track_id, :title, :track_number, :duration_ms
 
-  def initialize (album_id, track_id, title, track_number, duration_ms)
-    @album_id = album_id
+  def initialize (track_id, title, track_number, duration_ms)
     @track_id = track_id
     @title = title
     @track_number = track_number
--- space-jams/runner_refactor.rb
+++ space-jams/runner_refactor.rb
@@ -1,26 +1,19 @@
 require 'csv'
-require 'pry'
 require 'set'
 require_relative 'album_refactor'
+require 'pry'
 
 
 albums = Set.new
 
 CSV.foreach('space_jams.csv', headers: true, header_converters: :symbol) do |row|
-  album = Album.find_or_create(row[:album_id], row[:album_name], row[:artists])
-  album.add_track(track[:track_id], track[:title], track[:track_number], track[:duration_ms])
+  album = Album.find_or_create(albums, row[:album_id].to_i,
+                               row[:album_name], row[:artists])
+
+  album.add_track(row[:track_id], row[:title], row[:track_number].to_i,
+                  row[:duration_ms].to_f)
 
   albums.add(album)
-  # album = albums.find { |a| a.id == track[:album_id] }
-  #
-  # # if the album hasn't been added to the albums array yet, add it
-  # if album.nil?
-  #   album = Album.new(track[:album_id], track[:album_name], track[:artists])
-  #   albums << album
-  # end
-
-  # add the track to the album's @tracks instance variable
-  # album.tracks << Track.new(track[:album_id], track[:track_id], track[:title], track[:track_number], track[:duration_ms])
 end
 
 # print out the summary for each album
