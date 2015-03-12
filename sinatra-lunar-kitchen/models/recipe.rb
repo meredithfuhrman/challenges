c555d77b9db4125314536fcf44e21e3a52ce422b
Meredith Fuhrman <meredith.fuhrman@gmail.com>
2015-03-12 14:02:26 -0400

    Mid-refactor

--- sinatra-lunar-kitchen/models/recipe.rb
+++ sinatra-lunar-kitchen/models/recipe.rb
@@ -1,5 +1,4 @@
 class Recipe
-#Constructor
   attr_reader :id, :name, :instructions, :description
 
   def initialize(id, name, instructions, description)
@@ -7,14 +6,11 @@ class Recipe
     @name = name
     @instructions = instructions
     @description = description
-    @ingredients = Array.new
   end
-
-#Class methods
+ 
   def self.all
     sql_recipe_name = "SELECT * FROM recipes ORDER BY recipes.name;"
-    recipes = self.db_connection { |conn| conn.exec(sql_recipe_name)}.to_a
-
+    recipes = db_connection { |conn| conn.exec_params(sql_recipe_name)}.to_a
     @recipes = []
     recipes.each do |recipe|
       @recipes << Recipe.new(recipe["id"], recipe["name"],recipe["instructions"],recipe["description"])
@@ -22,16 +18,16 @@ class Recipe
     @recipes
   end
 
-
-  def self.find(recipe_id)
-    @recipes.each do |row|
-      return row if row.id == recipe_id
-    end
-    Recipe.new(recipe_id, "Error", "This recipe doesn't have any instructions.",
+  def self.find(id)
+    sql = "SELECT * FROM recipes WHERE id = $1"
+    recipe = db_connection { |conn| conn.exec_params(sql, [id])}.first
+    if 
+    Recipe.new(id, "Error", "This recipe doesn't have any instructions.",
           "This recipe doesn't have a description.")
   end
 
   def ingredients
+    @ingredients = Array.new
     Ingredient.all
     id = self.id
     @ingredients = Ingredient.find(id)
